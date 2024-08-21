a70dfc21a13390144ad8557047e94586e2367465
Peo2 <ajeigbefelix033@gmail.com>
2024-08-21 08:06:28 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { ApiFetch } from './Component/Display';
+import { Loading } from './Component/Load';
 
 
 
 function App() {
+  const[isloading, setIsLoading] = useState(true);
+  useEffect (() => {
+setTimeout(() => {
+  setIsLoading(false);
+}, 3000)
+  },[])
   return (
    
       <div className='card_container'>
-      <ApiFetch/>
+        {isloading ? (<Loading/>) :
+      <ApiFetch/>}
     </div>
     
   );
---
+++ src/Component/Load.jsx
@@ -0,0 +1,9 @@
+import './Load.css'
+
+export const Loading = () =>{
+    return(
+        <div className='load'>
+            <p>Kindly wait the web is loading...</p>
+        </div>
+    )
+}
